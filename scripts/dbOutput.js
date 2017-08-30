9040e8f6273ab8354e85774744aeb002e938b2dc
rayjune <ray_june@qq.com>
2017-08-30 18:51:57 +0800

    use ESlint to check code

--- scripts/dbOutput.js
+++ scripts/dbOutput.js
@@ -1,35 +1,35 @@
 //添加数据
 function addData(data) {
-    var transaction = db.transaction(["user"], "readwrite"),
-        storeHander = transaction.objectStore('user');
-    var addOpt = storeHander.add(data);
-    addOpt.onerror = function() {
-        console.log("failed");
-    }
-    addOpt.onsuccess = function() {
-        console.log("add success");
-    };
+	var transaction = db.transaction(['user'], 'readwrite'),
+		storeHander = transaction.objectStore('user');
+	var addOpt = storeHander.add(data);
+	addOpt.onerror = function() {
+		console.log('failed');
+	};
+	addOpt.onsuccess = function() {
+		console.log('add success');
+	};
 }
 
 //TODO:删除全部数据
 function delData() {
-    var transaction = request.result.transaction(["user"], "readwrite"),
-        storeHander = transaction.objectStore('user');
-    var range = IDBKeyRange.lowerBound(0, true);
-    storeHander.openCursor(range, 'next').onsuccess = function(e) {
-        var cursor = e.target.result;
-        if (cursor) {
-            var requestDel = cursor.delete();
-            requestDel.onsuccess = function() {
-                console.log("del success");
-                showData();
-            }
-            requestDel.onerror = function() {
-                console.log("del fail");
-            }
-            cursor.continue();
-        } else {}
-    };
+	var transaction = request.result.transaction(['user'], 'readwrite'),
+		storeHander = transaction.objectStore('user');
+	var range = IDBKeyRange.lowerBound(0, true);
+	storeHander.openCursor(range, 'next').onsuccess = function(e) {
+		var cursor = e.target.result;
+		if (cursor) {
+			var requestDel = cursor.delete();
+			requestDel.onsuccess = function() {
+				console.log('del success');
+				showData();
+			};
+			requestDel.onerror = function() {
+				console.log('del fail');
+			};
+			cursor.continue();
+		} else {}
+	};
 }
 
 // 删除某个数据
@@ -58,55 +58,55 @@ function delData() {
 
 //展示数据
 function showData() {
-    clearAllNodes();
-    var transaction = db.transaction(["user"], "readwrite"),
-        storeHander = transaction.objectStore('user');
+	clearAllNodes();
+	var transaction = db.transaction(['user'], 'readwrite'),
+		storeHander = transaction.objectStore('user');
 
-    var range = IDBKeyRange.lowerBound(0, true);
-    storeHander.openCursor(range, 'next').onsuccess = function(e) {
-        var cursor = e.target.result;
-        if (cursor) {
-            refreshNode(cursor.value);
-            console.log(cursor.value + "show");
-            cursor.continue();
-        } else {
-            console.log("done");
-        }
-    };
+	var range = IDBKeyRange.lowerBound(0, true);
+	storeHander.openCursor(range, 'next').onsuccess = function(e) {
+		var cursor = e.target.result;
+		if (cursor) {
+			refreshNode(cursor.value);
+			console.log(cursor.value + 'show');
+			cursor.continue();
+		} else {
+			console.log('done');
+		}
+	};
 }
 //显示已经完成的数据
 function showDataDone() {
-    clearAllNodes();
-    var transaction = db.transaction(["user"], "readwrite"),
-        storeHander = transaction.objectStore('user');
+	clearAllNodes();
+	var transaction = db.transaction(['user'], 'readwrite'),
+		storeHander = transaction.objectStore('user');
 
-    var range = IDBKeyRange.lowerBound(0, true);
-    storeHander.openCursor(range, 'next').onsuccess = function(e) {
-        var cursor = e.target.result;
-        if (cursor) {
-            if (cursor.value.finished) {
-                console.log(cursor.value + "done");
-                refreshNode(cursor.value);
-            }
-            cursor.continue();
-        } else {}
-    };
+	var range = IDBKeyRange.lowerBound(0, true);
+	storeHander.openCursor(range, 'next').onsuccess = function(e) {
+		var cursor = e.target.result;
+		if (cursor) {
+			if (cursor.value.finished) {
+				console.log(cursor.value + 'done');
+				refreshNode(cursor.value);
+			}
+			cursor.continue();
+		} else {}
+	};
 }
 
 //显示未完成的数据
 function showDataTodo() {
-    clearAllNodes();
-    var transaction = db.transaction(["user"], "readwrite"),
-        storeHander = transaction.objectStore('user');
+	clearAllNodes();
+	var transaction = db.transaction(['user'], 'readwrite'),
+		storeHander = transaction.objectStore('user');
 
-    var range = IDBKeyRange.lowerBound(0, true);
-    storeHander.openCursor(range, 'next').onsuccess = function(e) {
-        var cursor = e.target.result;
-        if (cursor) {
-            if (!cursor.value.finished) {
-                refreshNode(cursor.value);
-            }
-            cursor.continue();
-        } else {}
-    };
+	var range = IDBKeyRange.lowerBound(0, true);
+	storeHander.openCursor(range, 'next').onsuccess = function(e) {
+		var cursor = e.target.result;
+		if (cursor) {
+			if (!cursor.value.finished) {
+				refreshNode(cursor.value);
+			}
+			cursor.continue();
+		}
+	};
 }
--- scripts/noIndexDB.js
+++ scripts/noIndexDB.js
@@ -1,118 +1,118 @@
 // 暂时不用这个文件
 (function(){
-    var i; //index
+	var i; //index
 
-    //为每个li后面加上关闭按钮「x」
-    function closeBtn() {
-        var myNodelist = document.getElementsByTagName('li');
-        for(i = 0; i < myNodelist.length; i++) {
-            var span = document.createElement('span');
-            var txt = document.createTextNode('\u00D7'); //unicode编码下的x符号
-            span.className = 'close';
-            span.appendChild(txt);
-            myNodelist[i].appendChild(span);
-        }
-    }
+	//为每个li后面加上关闭按钮「x」
+	function closeBtn() {
+		var myNodelist = document.getElementsByTagName('li');
+		for(i = 0; i < myNodelist.length; i++) {
+			var span = document.createElement('span');
+			var txt = document.createTextNode('\u00D7'); //unicode编码下的x符号
+			span.className = 'close';
+			span.appendChild(txt);
+			myNodelist[i].appendChild(span);
+		}
+	}
 
-    //点击关闭按钮，隐藏当前li
-    function closeElement() {
-        var close = document.getElementsByClassName('close');
-        for(i = 0; i < close.length; i++) {
-            close[i].onclick = function() {
-                var div = this.parentElement; //关闭按钮的父元素
-                div.style.display = 'none';
-            }
-        }
-    }
+	//点击关闭按钮，隐藏当前li
+	function closeElement() {
+		var close = document.getElementsByClassName('close');
+		for(i = 0; i < close.length; i++) {
+			close[i].onclick = function() {
+				var div = this.parentElement; //关闭按钮的父元素
+				div.style.display = 'none';
+			};
+		}
+	}
 
-    //点击li的时候，加上.checked，再点击则取消
-    function ifChecked() {
-        var list = document.getElementsByTagName('ul')[0];
-        list.onclick = function(event) {
-            if(event.target.tagName === "LI") {
-                event.target.classList.toggle('checked');
-            }
-        }
-    }
+	//点击li的时候，加上.checked，再点击则取消
+	function ifChecked() {
+		var list = document.getElementsByTagName('ul')[0];
+		list.onclick = function(event) {
+			if(event.target.tagName === 'LI') {
+				event.target.classList.toggle('checked');
+			}
+		};
+	}
 
-    Date.prototype.Format = function(fmt) {
-        var o = {
-            "y+": this.getFullYear(),
-            "M+": this.getMonth() + 1, //月份
-            "d+": this.getDate(), //日
-        };
-        for (var k in o) {
-            if (new RegExp("(" + k + ")").test(fmt)) {
-                if (k == "y+") {
-                    fmt = fmt.replace(RegExp.$1, ("" + o[k]).substr(4 - RegExp.$1.length));
-                } else if (k == "S+") {
-                    var lens = RegExp.$1.length;
-                    lens = lens == 1 ? 3 : lens;
-                    fmt = fmt.replace(RegExp.$1, ("00" + o[k]).substr(("" + o[k]).length - 1, lens));
-                } else {
-                    fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
-                }
-            }
-        }
-        return fmt;
-    }
+	Date.prototype.Format = function(fmt) {
+		var o = {
+			'y+': this.getFullYear(),
+			'M+': this.getMonth() + 1, //月份
+			'd+': this.getDate(), //日
+		};
+		for (var k in o) {
+			if (new RegExp('(' + k + ')').test(fmt)) {
+				if (k == 'y+') {
+					fmt = fmt.replace(RegExp.$1, ('' + o[k]).substr(4 - RegExp.$1.length));
+				} else if (k == 'S+') {
+					var lens = RegExp.$1.length;
+					lens = lens == 1 ? 3 : lens;
+					fmt = fmt.replace(RegExp.$1, ('00' + o[k]).substr(('' + o[k]).length - 1, lens));
+				} else {
+					fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (('00' + o[k]).substr(('' + o[k]).length)));
+				}
+			}
+		}
+		return fmt;
+	};
 
-    //点击添加时，创建一个新的ul
-    function newElement() {
-        var li = document.createElement('li');
-        var inputValue = document.getElementById('myInput').value;
-        var createDate = new Date();
-        var date = createDate.Format("yyyy/MM/dd:  ");
-        inputValue = date  + " " + " " + inputValue;
-        var t = document.createTextNode(inputValue);
-        li.appendChild(t);
-        if (inputValue === '') {
-            alert('亲，请输入一个具体的任务');
-        }
-        else {
-            document.getElementById('myUl').insertBefore(li, document.getElementById('myUl').firstChild);
-        }
-        document.getElementById('myInput').value = ''; //清空input输入框
-    }
+	//点击添加时，创建一个新的ul
+	function newElement() {
+		var li = document.createElement('li');
+		var inputValue = document.getElementById('myInput').value;
+		var createDate = new Date();
+		var date = createDate.Format('yyyy/MM/dd:  ');
+		inputValue = date  + ' ' + ' ' + inputValue;
+		var t = document.createTextNode(inputValue);
+		li.appendChild(t);
+		if (inputValue === '') {
+			alert('亲，请输入一个具体的任务');
+		}
+		else {
+			document.getElementById('myUl').insertBefore(li, document.getElementById('myUl').firstChild);
+		}
+		document.getElementById('myInput').value = ''; //清空input输入框
+	}
 
-    //清除TODO显示面板上所有的节点
-    function clearAllNodes() {
-        var root = document.getElementById('myUl');
-        while (root.hasChildNodes()) {
-            root.removeChild(root.firstChild);
-        }
-    }
+	//清除TODO显示面板上所有的节点
+	function clearAllNodes() {
+		var root = document.getElementById('myUl');
+		while (root.hasChildNodes()) {
+			root.removeChild(root.firstChild);
+		}
+	}
 
-    //初始化list列表
-    function initList() {
-        closeBtn();
-        closeElement();
-        ifChecked();
-    }
+	//初始化list列表
+	function initList() {
+		closeBtn();
+		closeElement();
+		ifChecked();
+	}
 
-    //初始化
-    function init() {
-        var addButton = document.getElementById('add');
-        var deleteButton = document.getElementById('delete');
-        initList();
+	//初始化
+	function init() {
+		var addButton = document.getElementById('add');
+		var deleteButton = document.getElementById('delete');
+		initList();
 
-        //添加按钮点击时执行
-        addButton.onclick = function() {
-            newElement();
-            initList();
-        }
-        deleteButton.onclick = function() {
-            clearAllNodes();
-        }
-        //按回车时同样执行添加
-        document.onkeydown = function(event) {
-            if(event.keyCode == 13) {
-                newElement();
-                initList();
-            }
-        }
-    }
+		//添加按钮点击时执行
+		addButton.onclick = function() {
+			newElement();
+			initList();
+		};
+		deleteButton.onclick = function() {
+			clearAllNodes();
+		};
+		//按回车时同样执行添加
+		document.onkeydown = function(event) {
+			if(event.keyCode == 13) {
+				newElement();
+				initList();
+			}
+		};
+	}
 
-    init();
+	init();
 
 })();

9e8cdef4fcf75edb2181a8025683972a5309b5b4
rayjune <ray_june@qq.com>
2017-09-01 15:53:46 +0800

    reconfig files & Minimizing Globals & use Single var Pattern

--- scripts/(弃用)noIndexDB.js
+++ scripts/(弃用)noIndexDB.js
@@ -4,10 +4,12 @@
 
 	//为每个li后面加上关闭按钮「x」
 	function closeBtn() {
-		var myNodelist = document.getElementsByTagName('li');
+		var myNodelist = document.getElementsByTagName('li'),
+			span,
+			txt;
 		for(i = 0; i < myNodelist.length; i++) {
-			var span = document.createElement('span');
-			var txt = document.createTextNode('\u00D7'); //unicode编码下的x符号
+			span = document.createElement('span');
+			txt = document.createTextNode('\u00D7'); //unicode编码下的x符号
 			span.className = 'close';
 			span.appendChild(txt);
 			myNodelist[i].appendChild(span);
@@ -16,10 +18,11 @@
 
 	//点击关闭按钮，隐藏当前li
 	function closeElement() {
-		var close = document.getElementsByClassName('close');
+		var close = document.getElementsByClassName('close'),
+			div;
 		for(i = 0; i < close.length; i++) {
 			close[i].onclick = function() {
-				var div = this.parentElement; //关闭按钮的父元素
+				div = this.parentElement; //关闭按钮的父元素
 				div.style.display = 'none';
 			};
 		}
@@ -37,16 +40,18 @@
 
 	Date.prototype.Format = function(fmt) {
 		var o = {
-			'y+': this.getFullYear(),
-			'M+': this.getMonth() + 1, //月份
-			'd+': this.getDate(), //日
-		};
-		for (var k in o) {
+				'y+': this.getFullYear(),
+				'M+': this.getMonth() + 1, //月份
+				'd+': this.getDate(), //日
+			},
+			k,
+			lens;
+		for ( k in o) {
 			if (new RegExp('(' + k + ')').test(fmt)) {
 				if (k == 'y+') {
 					fmt = fmt.replace(RegExp.$1, ('' + o[k]).substr(4 - RegExp.$1.length));
 				} else if (k == 'S+') {
-					var lens = RegExp.$1.length;
+					lens = RegExp.$1.length;
 					lens = lens == 1 ? 3 : lens;
 					fmt = fmt.replace(RegExp.$1, ('00' + o[k]).substr(('' + o[k]).length - 1, lens));
 				} else {
@@ -59,13 +64,14 @@
 
 	//点击添加时，创建一个新的ul
 	function newElement() {
-		var li = document.createElement('li');
-		var inputValue = document.getElementById('myInput').value;
-		var createDate = new Date();
-		var date = createDate.Format('yyyy/MM/dd:  ');
+		var li = document.createElement('li'),
+			inputValue = document.getElementById('myInput').value,
+			createDate = new Date(),
+			date = createDate.Format('yyyy/MM/dd:  '),
+			txt;
 		inputValue = date  + ' ' + ' ' + inputValue;
-		var t = document.createTextNode(inputValue);
-		li.appendChild(t);
+		txt = document.createTextNode(inputValue);
+		li.appendChild(txt);
 		if (inputValue === '') {
 			alert('亲，请输入一个具体的任务');
 		}
@@ -92,8 +98,8 @@
 
 	//初始化
 	function init() {
-		var addButton = document.getElementById('add');
-		var deleteButton = document.getElementById('delete');
+		var addButton = document.getElementById('add'),
+			deleteButton = document.getElementById('delete');
 		initList();
 
 		//添加按钮点击时执行
---
+++ scripts/otherFun.js
@@ -0,0 +1,128 @@
+//更新节点的操作
+function refreshNode(user_object) {
+	//在网页上更新数据
+	//新建文本节点,记录时间和事件的节点，然后给文本节点赋值
+	var dataParent = document.getElementById('myUl'),
+	    textDate = document.createTextNode(user_object.user_date + ': ' ),
+	    textEvent = document.createElement('span'),
+		text = document.createTextNode(' ' + user_object.user_event),
+		li = document.createElement('li'),
+		i; //index
+	textEvent.appendChild(text);
+	//根据完成的情况添加样式
+	//完成的情况
+	if (user_object.finished) {
+		li.classList.add('checked');
+	} 
+	//把文本节点附加上
+	li.appendChild(textDate);
+	li.appendChild(textEvent);
+	//节点附加
+	dataParent.insertBefore(li, dataParent.firstChild); //插入到最前面
+        
+        
+	//为每个li后面加上关闭按钮「x」
+	(function closeBtn() {
+		var myNodelist = document.getElementsByTagName('li');
+		for(i = 0; i < myNodelist.length; i++) {
+			var span = document.createElement('span'),
+			    txt = document.createTextNode('\u00D7'); //unicode编码下的x符号
+			span.className = 'close';
+			span.appendChild(txt);
+			myNodelist[i].appendChild(span);
+		}
+	})();
+    
+	//点击关闭按钮，删除当前li
+	(function closeElement() {
+		var close = document.getElementsByClassName('close'),
+			div;
+		for(i = 0; i < close.length; i++) {
+			close[i].onclick = function() {
+				div = this.parentElement; //关闭按钮的父元素
+				div.style.display = 'none';
+			};
+		}
+	})();
+    
+	//添加点击事件
+	li.addEventListener('click', function() {
+		user_object.finished = !user_object.finished;
+		//完成的情况
+		if (user_object.finished) {
+			li.classList.add('checked');
+		}
+		//未完成的情况
+		else {
+			li.classList.remove('checked');
+		}
+    
+		//把数据同步到数据库
+		var transaction = db.transaction(['user'], 'readwrite'),
+			storeHander = transaction.objectStore('user');
+		user_object.finished = user_object.finished;
+		console.log(user_object);
+		//因为ID是自动增长的，所以使用put会给他增加数据，而不是修改数据
+		storeHander.put(user_object).onsuccess = function(e) {
+			console.log('修改数据成功');
+		};
+    
+	}, false);
+}
+
+//格式化日期
+Date.prototype.Format = function(fmt) {
+	var o = {
+		'y+': this.getFullYear(),
+		'M+': this.getMonth() + 1, //月份
+		'd+': this.getDate(), //日
+		'h+': this.getHours(), //小时
+		'm+': this.getMinutes() //分
+	};
+	for (var k in o) {
+		if (new RegExp('(' + k + ')').test(fmt)) {
+			if (k == 'y+') {
+				fmt = fmt.replace(RegExp.$1, ('' + o[k]).substr(4 - RegExp.$1.length));
+			} else if (k == 'S+') {
+				var lens = RegExp.$1.length;
+				lens = lens == 1 ? 3 : lens;
+				fmt = fmt.replace(RegExp.$1, ('00' + o[k]).substr(('' + o[k]).length - 1, lens));
+			} else {
+				fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (('00' + o[k]).substr(('' + o[k]).length)));
+			}
+		}
+	}
+	return fmt;
+};
+    
+//加载前要清除TODO显示面板上所有的节点
+function clearAllNodes() {
+	var root = document.getElementById('myUl');
+	while (root.hasChildNodes()) {
+		root.removeChild(root.firstChild); //这种清除子节点速度非常快
+	}
+}
+    
+//清除输入框中的数据
+function clearInput() {
+	var inputComment = document.getElementById('myInput');
+	inputComment.value = '';
+}
+        
+//获取输入的数据,并对数据进行处理
+function setEvent() {
+	console.log(user_id);
+        
+	var item = document.getElementById('myInput').value;
+	var createDate = new Date();
+	var date = createDate.Format('yyyy年MM月dd日 hh:mm');
+	user_id++;
+	var arrangement = {
+		id: user_id,
+		user_event: item,
+		finished: false,
+		user_date: date
+	};
+	console.log(arrangement);
+	return arrangement;
+}

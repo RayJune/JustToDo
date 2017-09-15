b90f258bde903b8bde2d9b55f96e78948ef64a92
rayjune <ray_june@qq.com>
2017-09-15 17:29:56 +0800

    Aha, accomplished a refactor again, mainly use event-delagation and add some comments to promote readability (Also make code looks beauty, although code is for human to see & understand). By using dev-tools's breakpoints fix some bugs (find out it's more powerful than console.log) , but still leave a showWhetherDone's bug. Plan to take a break and fix it.

--- scripts/myToDo.js
+++ scripts/myToDo.js
@@ -1,5 +1,8 @@
 // 处理浏览器兼容性
 (function myToDo() {
+  /* 配置数据库，并在数据库调用成功后开始添加事件处理函数  */
+
+  //首先检测浏览器indexedDB可用性
   if (!window.indexedDB) {
     window.alert('Your browser doesn\'t support a stable version of IndexedDB. Such and such feature will not be available.');
   }
@@ -7,30 +10,30 @@
     dbname: 'justToDo',
     dbVersion: '1'
   };
+  var db;
+  var userId;
   var request = indexedDB.open(cfg.dbname, cfg.dbVersion); // 打开数据库
 
   request.onerror = function error() {
     console.log('indexDB加载失败');
   };
-  // 异步成功后才能获取到
+  // 异步处理成功后才能获取到
   request.onsuccess = function success(event) {
     db = event.target.result;
     showData(); // 将数据展示
-    user_id = getId();
+    userId = getId();
     // 添加事件处理函数
     document.getElementById('add').addEventListener('click', addOneList, false);
-    document.addEventListener('keydown', function handleEnterEvent(e) {
-      if (e.keyCode === 13) {
-        addOneList();
-      }
-    });
+    document.addEventListener('keydown', handleEnterEvent, true);
+    document.querySelector('#myUl').addEventListener('click', handleLiClickDelagation, false);
+    document.querySelector('#myUl').addEventListener('click', handleXClickDelagation, false);
     document.getElementById('done').addEventListener('click', showDataDone, false);
     document.getElementById('todo').addEventListener('click', showDataTodo, false);
     document.getElementById('all').addEventListener('click', showData, false);
     document.getElementById('delete').addEventListener('click', deleteAllData, false);
   };
 
-  request.onupgradeneeded = function open(event) { // 在我们请求打开的数据库的版本号和已经存在的数据库版本号不一致的时候调用。
+  request.onupgradeneeded = function schemaChanged(event) { // 在我们请求打开的数据库的版本号和已经存在的数据库版本号不一致的时候调用。
     db = event.target.result;
     if (!db.objectStoreNames.contains('user')) {
       // 在这里可以设置键值，也可以是auto
@@ -39,13 +42,16 @@
     // 在这里新建好一个数据库demo
     store.add({
       id: 0,
-      user_event: 0,
+      userEvent: 0,
       finished: true,
       date: 0
     });
   };
 
-  // 获取到现在的ID值
+
+  /* 经常调用的函数  */
+
+  // 获取当前的ID值
   function getId() {
     var transaction = db.transaction(['user'], 'readwrite');
     var storeHander = transaction.objectStore('user');
@@ -54,15 +60,14 @@
       var cursor = e.target.result;
       if (cursor) {
         cursor.continue();
-        user_id = cursor.value.id;
+        userId = cursor.value.id;
       } else {
-        console.log(user_id);
+        console.log('现在的id为:' + userId);
       }
     };
   }
 
-  // 删除所有数据
-
+  // 重置所有节点为0
   function resetNodes() { // 重置ul为0
     var root = document.querySelector('#myUl');
     while (root.hasChildNodes()) {
@@ -70,84 +75,7 @@
     }
   }
 
-  function refreshOneNode(data) { // 更新一个list节点，并返回一个fragment
-    var fragment = document.createDocumentFragment();	// 使用fragment来包裹节点
-    var textDate = document.createTextNode(data.user_date + ': ');
-    var textWrap = document.createElement('span');
-    var text = document.createTextNode(' ' + data.user_event);
-    var li = document.createElement('li');
-    textWrap.appendChild(text);
-    li.appendChild(textDate);
-    li.appendChild(textWrap);
-
-    // 根据完成的情况来确定是否添加完成样式
-    if (data.finished) {
-      li.classList.add('checked');
-    }
-
-    // 给每个li后面加上关闭按钮，并添加【x】删除事件
-    var span = document.createElement('span');
-    var x = document.createTextNode('\u00D7'); // unicode下的【x】
-    span.className = 'close';
-    span.appendChild(x);
-    span.addEventListener('click', function deleteOneData() {
-      this.parentElement.style.display = 'none';	// 先隐藏起来
-
-      // 再更新数据库
-      var transaction = db.transaction(['user'], 'readwrite');
-      var storeHander = transaction.objectStore('user');
-      var deleteOpt = storeHander.delete(data.id); // 将当前选中li的数据从数据库中删除
-      deleteOpt.onerror = function error() {
-        console.log('删除' + data.id + '到数据库失败');
-      };
-      deleteOpt.onsuccess = function success() {
-        console.log('删除' + data.id +  '到数据库成功');
-      };
-    });
-    li.appendChild(span);
-
-    // 添加点击事件
-    li.addEventListener('click', function switchLi() {
-      data.finished = !data.finished; // 切换
-      if (data.finished) {
-        li.classList.add('checked');
-      } else {
-        li.classList.remove('checked');
-      }
-
-      // 把数据同步到数据库
-      var transaction = db.transaction(['user'], 'readwrite');
-      var storeHander = transaction.objectStore('user');
-      data.finished = data.finished;
-      console.log(data);
-      // 因为ID是自动增长的，所以使用put会给他增加数据，而不是修改数据
-      storeHander.put(data).onsuccess = function  updateFinishedData() {
-        console.log('修改数据成功');
-      };
-    });
-    fragment.appendChild(li);
-    return li;
-  }
-
-  function refreshNodes(dataArr) { // 刷新节点
-    var parent = document.querySelector('#myUl');
-    var fragment = document.createDocumentFragment(); // 创建fragment
-    var i;
-    var len = dataArr.length;
-    for (i = 0; i < len; i++) {
-      fragment.insertBefore(refreshOneNode(dataArr[i]), fragment.firstChild); // 每一个新加入的元素都排在最前面
-      console.log(fragment.firstChild);
-      // 为每个节点添加data-index属性值
-      if (!fragment.firstChild.getAttribute('data-index')) {
-        fragment.firstChild.setAttribute('data-index', dataArr[i].id);
-      }
-      console.log(fragment.firstChild);
-    }
-    // 将fragment添加到DOM中
-    parent.appendChild(fragment);
-    // 
-  }
-
+  // showData同时也是all的事件处理函数
   function showData() { // 取出并展示所有list数据
     resetNodes(); // 先重置ul
     var transaction = db.transaction(['user'], 'readwrite');
@@ -167,8 +95,96 @@
     };
   }
 
+  function refreshNodes(dataArr) { // 刷新一组节点
+    var parent = document.querySelector('#myUl');
 
-  // 格式化日期
+    // 利用fragment来包裹li们，这样可以将多次DOM操作减少为一次DOM操作
+    var fragment = document.createDocumentFragment(); 
+    var i;
+    var len = dataArr.length;
+    for (i = 0; i < len; i++) {
+      fragment.insertBefore(refreshOneNode(dataArr[i]), fragment.firstChild); // 每一个新加入的元素都排在最前面
+    }
+    // 将fragment添加到DOM中，只操纵这一次DOM
+    parent.appendChild(fragment);
+    console.log('数据添加到DOM完毕');
+  }
+
+  function refreshOneNode(data) { // 刷新一个list节点，并返回一个fragment
+    var textDate = document.createTextNode(data.userDate + ': ');
+    var textWrap = document.createElement('span');
+    var text = document.createTextNode(' ' + data.userEvent);
+    var li = document.createElement('li');
+    textWrap.appendChild(text);
+    li.appendChild(textDate);
+    li.appendChild(textWrap);
+
+    // 根据完成的情况来确定是否添加完成样式
+    if (data.finished) {
+      li.classList.add('checked');
+    }
+
+    // 给每个li后面加上关闭按钮，并添加【x】删除事件
+    var span = document.createElement('span');
+    var x = document.createTextNode('\u00D7'); // unicode下的【x】
+    span.className = 'close';
+    span.appendChild(x);
+    // 为每个[x]添加data-x属性值
+    span.setAttribute('data-x', data.id);
+    li.appendChild(span);
+    // 为每个节点添加data-index属性值
+    if (!li.getAttribute('data-index')) {
+      li.setAttribute('data-index', data.id);
+    }
+    return li;
+  }
+
+
+  /* add的事件处理函数  */
+
+  // 添加一条新list数据
+  function addOneList() {
+    // 首先获取输入框中的数据
+    var input = document.querySelector('#myInput');
+    var value = input.value;
+    var date = getNewDate('yyyy年MM月dd日 hh:mm');
+    userId++;
+    if (value === '') {
+      alert('请亲传入数据后重新提交~');
+      return false;
+    }
+    // 整合为一个完整的数据
+    var arrangement = {
+      id: userId,
+      userEvent: value,
+      finished: false,
+      userDate: date
+    };
+
+    // 添加list数据到数据库中
+    var transaction = db.transaction(['user'], 'readwrite');
+    var storeHander = transaction.objectStore('user');
+    var addOpt = storeHander.add(arrangement);
+    addOpt.onerror = function error() {
+      console.log('添加到数据库失败');
+    };
+    addOpt.onsuccess = function success() {
+      console.log('添加到数据库成功');
+      console.log('您添加的数据为：' + arrangement.userDate + ':  ' + arrangement.userEvent); // 打印一下写入的数据
+    };
+
+    // 添加节点
+    var newNode = refreshOneNode(arrangement);
+    newNode.setAttribute('data-index', arrangement.id);
+    var parent = document.querySelector('#myUl');
+    parent.insertBefore(newNode, parent.firstChild);
+
+    // 重置输入框为0
+    input.value = '';
+    return 0;
+  }
+  
+  // 格式化日期，用来格式化li中的日期
   function getNewDate(fmt) {
     var newDate = new Date();
     // var newfmt;
@@ -192,58 +208,111 @@
         }
       }
     }
-    return fmt; // FIXME: wrong date parse: yyyy年MM月dd日 hh:26: 
+    return fmt;
+  }
+  
+
+  /* 键盘回车添加list的事件处理函数  */
+
+  function handleEnterEvent(e) {
+    if (e.keyCode === 13) {
+      addOneList();
+    }
   }
 
-  // 添加一条新list数据
-  function addOneList() {
-    // 首先获取输入框中的数据
-    var input = document.querySelector('#myInput');
-    var value = input.value;
-    var date = getNewDate('yyyy年MM月dd日 hh:mm');
-    user_id++;
-    if (value === '') {
-      alert('请亲传入数据后重新提交~');
-      return false;
-    }
-    // 整合为一个完整的数据
-    var arrangement = {
-      id: user_id,
-      user_event: value,
-      finished: false,
-      user_date: date
-    };
-    console.log('您添加的数据为：' + arrangement); // 打印一下写入的数据
 
-    // 添加list数据到数据库中
+  /* 点击li的事件处理函数  */
+  
+  // 利用事件代理，将本来绑定在每个li上的事件处理函数绑定在ul上
+  function handleLiClickDelagation(e) {
+    var nodeData;
+    if (e.target.getAttribute('data-index')) {
+      var dataIndex = e.target.getAttribute('data-index'); // 获得对应id值
+      dataIndex = parseInt(dataIndex, 10); // 将字符串转化为数字，方便查询
+
+      var transaction = db.transaction(['user']);
+      var storeHander = transaction.objectStore('user');
+      var getDataIndex = storeHander.get(dataIndex);  // 在数据库中获取到相应的对象数值
+      var that = e.target;
+      getDataIndex.onerror = function getDataIndexError() {
+        console.log('failed');
+      };
+      getDataIndex.onsuccess = function getDataIndexSuccess() {
+        console.log('查找数据成功');
+        nodeData = getDataIndex.result;
+        switchLi(nodeData, that);
+      };
+    }
+  }
+
+  function switchLi(data, that) {
+    that.finished = !data.finished; // 切换
+    if (that.finished) {
+      that.classList.add('checked');
+    } else {
+      that.classList.remove('checked');
+    }
+
+    // 把数据同步到数据库
     var transaction = db.transaction(['user'], 'readwrite');
     var storeHander = transaction.objectStore('user');
-    var addOpt = storeHander.add(arrangement);
-    addOpt.onerror = function error() {
-      console.log('添加到数据库失败');
+    data.finished = that.finished;
+    console.log(data);
+    // 因为ID是自动增长的，所以使用put会给他增加数据，而不是修改数据
+    var putStore = storeHander.put(data);
+    putStore.onerror = function putStoreError() {
+      console.log('修改数据失败');
+    }
+    putStore.onsuccess = function putStoreSuccess() {
+      console.log('修改数据成功');
     };
-    addOpt.onsuccess = function success() {
-      console.log('添加到数据库成功');
-    };
-
-    // 添加节点
-    var newNode = refreshOneNode(arrangement);
-    newNode.setAttribute('data-index', arrangement.id);
-    var parent = document.querySelector('#myUl');
-    parent.insertBefore(newNode, parent.firstChild);
-
-    // 重置输入框为0
-    input.value = '';
-    return 0;
   }
 
-  // 显示所有 已/未 完成的list
+
+  /* li上[x]点击的事件处理函数（删除这一条list）  */
+  
+  function handleXClickDelagation(e) {
+    if (e.target.className === 'close') {
+      var dataX = e.target.getAttribute('data-x');
+      dataX = parseInt(dataX, 10);
+      var transaction = db.transaction(['user']);
+      var storeHander = transaction.objectStore('user');
+      var getDataIndex = storeHander.get(dataX);
+      getDataIndex.onerror = function getDataIndexError() {
+        console.log('failed');
+      };
+      getDataIndex.onsuccess = function getDataIndexSuccess() {
+        console.log('查找数据成功');
+        nodeData = getDataIndex.result;
+        deleteOneData(nodeData);
+      };
+    }
+  }
+
+  function deleteOneData(nodeData) {
+    var transaction = db.transaction(['user'], 'readwrite');
+    var storeHander = transaction.objectStore('user');
+    var deleteOpt = storeHander.delete(nodeData.id); // 将当前选中li的数据从数据库中删除
+    deleteOpt.onerror = function error() {
+      console.log('删除' + nodeData.id + '到数据库失败');
+    };
+    deleteOpt.onsuccess = function success() {
+      console.log('删除' + nodeData.id +  '到数据库成功');
+    };
+    showData(); // 从修改后的数据库中重新展示list
+  }
+
+
+  /* 显示所有 已/未 完成list的事件处理函数  */
+  
   function showWhetherDone(whether) {
     resetNodes(); // 重置ul
+
+    // TODO: 在这里打上断点，如果在断点中一直点击下一步运行则最后数据会展示出来，否则没有断点，正常操作则不显示数据
+    var dataArr = []; // 用数组来存储每一条符合要求的数据，最后再统一加入文档节点
     var transaction = db.transaction(['user'], 'readwrite');
     var storeHander = transaction.objectStore('user');
     var range = IDBKeyRange.lowerBound(0, true);
-    var dataArr = [];
     storeHander.openCursor(range, 'next').onsuccess = function showWhetherDoneData(e) {
       var cursor = e.target.result;
       if (cursor) {
@@ -258,8 +327,9 @@
         }
         cursor.continue();
       }
-      refreshNodes(dataArr);  // 将符合条件的li数据整合为数组传入refreshNodes函数
     };
+    refreshNodes(dataArr);  // 将符合条件的li数据整合为数组传入refreshNodes函数
+    console.log('显示数据完毕');
   }
 
   // 显示所有已完成的list
@@ -271,6 +341,9 @@
   function showDataTodo() {
     showWhetherDone(false);
   }
+
+
+  /* 删除所有数据的事件处理函数  */
 
   // 删除所有list数据
   function deleteAllData() {

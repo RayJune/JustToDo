69b6d4dcea8220e306eed8170a075530bf6e4981
rayjune <ray_june@qq.com>
2018-01-18 21:59:02 +0800

    use browserify to bundle template.js, reduce file request

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,7 +4,9 @@
   var config = require('./db/config.js');
   var addEvents = require('./utlis/addEvents/dbSuccess');
   var lazyLoadWithoutDB = require('./utlis/lazyLoadWithoutDB');
+  var templete = require('../../templete/template.js');
 
+  templete();
   // open DB, and when DB open succeed, invoke initial function
   DB.open(config, addEvents, lazyLoadWithoutDB);
 }());
--- templete/template.js
+++ templete/template.js
@@ -1,1 +1,42 @@
-!function(){var n=Handlebars.template;(Handlebars.templates=Handlebars.templates||{}).li=n({1:function(n,l,a,e,t){var s;return'  <li class="aphorism">'+n.escapeExpression((s=null!=(s=a.sentence||(null!=l?l.sentence:l))?s:a.helperMissing,"function"==typeof s?s.call(null!=l?l:n.nullContext||{},{name:"sentence",hash:{},data:t}):s))+"</li>\n"},3:function(n,l,a,e,t){var s;return null!=(s=a.each.call(null!=l?l:n.nullContext||{},null!=l?l.listItems:l,{name:"each",hash:{},fn:n.program(4,t,0),inverse:n.noop,data:t}))?s:""},4:function(n,l,a,e,t){var s;return null!=(s=a.if.call(null!=l?l:n.nullContext||{},null!=l?l.finished:l,{name:"if",hash:{},fn:n.program(5,t,0),inverse:n.program(7,t,0),data:t}))?s:""},5:function(n,l,a,e,t){var s,i=null!=l?l:n.nullContext||{},u=a.helperMissing,r=n.escapeExpression;return'      <li class="finished" data-id='+r((s=null!=(s=a.id||(null!=l?l.id:l))?s:u,"function"==typeof s?s.call(i,{name:"id",hash:{},data:t}):s))+">\n        "+r((s=null!=(s=a.date||(null!=l?l.date:l))?s:u,"function"==typeof s?s.call(i,{name:"date",hash:{},data:t}):s))+" : \n        <span>"+r((s=null!=(s=a.event||(null!=l?l.event:l))?s:u,"function"==typeof s?s.call(i,{name:"event",hash:{},data:t}):s))+'</span>\n        <span class="close">×</span>\n      </li>\n'},7:function(n,l,a,e,t){var s,i=null!=l?l:n.nullContext||{},u=a.helperMissing,r=n.escapeExpression;return"      <li data-id="+r((s=null!=(s=a.id||(null!=l?l.id:l))?s:u,"function"==typeof s?s.call(i,{name:"id",hash:{},data:t}):s))+">\n        "+r((s=null!=(s=a.date||(null!=l?l.date:l))?s:u,"function"==typeof s?s.call(i,{name:"date",hash:{},data:t}):s))+" : \n        <span>"+r((s=null!=(s=a.event||(null!=l?l.event:l))?s:u,"function"==typeof s?s.call(i,{name:"event",hash:{},data:t}):s))+'</span>\n        <span class="close">×</span>\n      </li>\n'},compiler:[7,">= 4.0.0"],main:function(n,l,a,e,t){var s;return null!=(s=a.if.call(null!=l?l:n.nullContext||{},null!=l?l.sentence:l,{name:"if",hash:{},fn:n.program(1,t,0),inverse:n.program(3,t,0),data:t}))?s:""},useData:!0})}();
+module.exports = function() {
+  var template = Handlebars.template, templates = Handlebars.templates = Handlebars.templates || {};
+templates['li'] = template({"1":function(container,depth0,helpers,partials,data) {
+    var helper;
+
+  return "  <li class=\"aphorism\">"
+    + container.escapeExpression(((helper = (helper = helpers.sentence || (depth0 != null ? depth0.sentence : depth0)) != null ? helper : helpers.helperMissing),(typeof helper === "function" ? helper.call(depth0 != null ? depth0 : (container.nullContext || {}),{"name":"sentence","hash":{},"data":data}) : helper)))
+    + "</li>\n";
+},"3":function(container,depth0,helpers,partials,data) {
+    var stack1;
+
+  return ((stack1 = helpers.each.call(depth0 != null ? depth0 : (container.nullContext || {}),(depth0 != null ? depth0.listItems : depth0),{"name":"each","hash":{},"fn":container.program(4, data, 0),"inverse":container.noop,"data":data})) != null ? stack1 : "");
+},"4":function(container,depth0,helpers,partials,data) {
+    var stack1;
+
+  return ((stack1 = helpers["if"].call(depth0 != null ? depth0 : (container.nullContext || {}),(depth0 != null ? depth0.finished : depth0),{"name":"if","hash":{},"fn":container.program(5, data, 0),"inverse":container.program(7, data, 0),"data":data})) != null ? stack1 : "");
+},"5":function(container,depth0,helpers,partials,data) {
+    var helper, alias1=depth0 != null ? depth0 : (container.nullContext || {}), alias2=helpers.helperMissing, alias3="function", alias4=container.escapeExpression;
+
+  return "      <li class=\"finished\" data-id="
+    + alias4(((helper = (helper = helpers.id || (depth0 != null ? depth0.id : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"id","hash":{},"data":data}) : helper)))
+    + ">\n        "
+    + alias4(((helper = (helper = helpers.date || (depth0 != null ? depth0.date : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"date","hash":{},"data":data}) : helper)))
+    + " : \n        <span>"
+    + alias4(((helper = (helper = helpers.event || (depth0 != null ? depth0.event : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"event","hash":{},"data":data}) : helper)))
+    + "</span>\n        <span class=\"close\">×</span>\n      </li>\n";
+},"7":function(container,depth0,helpers,partials,data) {
+    var helper, alias1=depth0 != null ? depth0 : (container.nullContext || {}), alias2=helpers.helperMissing, alias3="function", alias4=container.escapeExpression;
+
+  return "      <li data-id="
+    + alias4(((helper = (helper = helpers.id || (depth0 != null ? depth0.id : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"id","hash":{},"data":data}) : helper)))
+    + ">\n        "
+    + alias4(((helper = (helper = helpers.date || (depth0 != null ? depth0.date : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"date","hash":{},"data":data}) : helper)))
+    + " : \n        <span>"
+    + alias4(((helper = (helper = helpers.event || (depth0 != null ? depth0.event : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"event","hash":{},"data":data}) : helper)))
+    + "</span>\n        <span class=\"close\">×</span>\n      </li>\n";
+},"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
+    var stack1;
+
+  return ((stack1 = helpers["if"].call(depth0 != null ? depth0 : (container.nullContext || {}),(depth0 != null ? depth0.sentence : depth0),{"name":"if","hash":{},"fn":container.program(1, data, 0),"inverse":container.program(3, data, 0),"data":data})) != null ? stack1 : "");
+},"useData":true});
+};
